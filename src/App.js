ff91ed2dcbd7e5088c7b57778c4d803f28ad3032
laytep <nazar.mishchenia@gmail.com>
2022-06-30 10:34:41 +0200

    Fix html "class" to react "className"

--- src/App.js
+++ src/App.js
@@ -8,22 +8,22 @@ import './scss/app.scss';
 function App() {
   return (
     <div className="App">
-      <div class="wrapper">
+      <div className="wrapper">
         <Header />
-        <div class="content">
-          <div class="container">
-            <div class="content__top">
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
               <Categories />
               <Sort />
             </div>
-            <h2 class="content__title">Bestsellers</h2>
-            <div class="content__items">
+            <h2 className="content__title">Bestsellers</h2>
+            <div className="content__items">
               <PizzaBlock title="Chiken pizza" price={55} />
               <PizzaBlock title="Cottage pizza" price={49} />
               <PizzaBlock title="Kebab pizza" price={67} />
             </div>
-            <h2 class="content__title">Hot deals</h2>
-            <div class="content__items">
+            <h2 className="content__title">Hot deals</h2>
+            <div className="content__items">
               <PizzaBlock />
               <PizzaBlock />
               <PizzaBlock />
