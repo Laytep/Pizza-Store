bd978035a1e61840df3fcdd1f826bcf67bf861cf
laytep <nazar.mishchenia@gmail.com>
2022-07-04 15:19:56 +0200

    Adaptive Layout 'major update'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
+import Cart from './pages/Cart';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import './scss/app.scss';
@@ -14,6 +15,7 @@ function App() {
           <div className="container">
             <Routes>
               <Route path="/" element={<Home />} />
+              <Route path="/cart" element={<Cart />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
           </div>
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,1 +1,5 @@
+const Cart = () => {
+  return <h1>Cart</h1>;
+};
 
+export default Cart;
