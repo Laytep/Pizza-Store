ba7500d6258111ab9f9ae7996da565fba98f3dbb
laytep <nazar.mishchenia@gmail.com>
2022-07-06 17:15:26 +0200

    Add search module

---
+++ src/components/SearchBlock/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Search.module.scss';
+
+const Search = () => {
+  return <input className={styles.root} placeholder="find pizza..." />;
+};
+
+export default Search;
