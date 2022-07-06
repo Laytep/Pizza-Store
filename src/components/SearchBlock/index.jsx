29e8a8913aba07011c1e030ed3a431a34503bb84
laytep <nazar.mishchenia@gmail.com>
2022-07-06 18:03:40 +0200

    add clear search icon and style

--- src/components/SearchBlock/index.jsx
+++ src/components/SearchBlock/index.jsx
@@ -1,3 +1,4 @@
+import { clear } from '@testing-library/user-event/dist/clear';
 import React from 'react';
 import styles from './Search.module.scss';
 
@@ -8,10 +9,17 @@ const Search = ({ searchValue, setSearchValue }) => {
         <path d="M18,10a8,8,0,1,0-3.1,6.31l6.4,6.4,1.41-1.41-6.4-6.4A8,8,0,0,0,18,10Zm-8,6a6,6,0,1,1,6-6A6,6,0,0,1,10,16Z" />
       </svg>
       <input
+        value={searchValue}
         onChange={(event) => setSearchValue(event.target.value)}
         className={styles.input}
         placeholder="find pizza..."
       />
+      <svg className={styles.clearIcon}>
+        <path
+          d="M14.8,12l3.6-3.6c0.8-0.8,0.8-2,0-2.8c-0.8-0.8-2-0.8-2.8,0L12,9.2L8.4,5.6c-0.8-0.8-2-0.8-2.8,0   c-0.8,0.8-0.8,2,0,2.8L9.2,12l-3.6,3.6c-0.8,0.8-0.8,2,0,2.8C6,18.8,6.5,19,7,19s1-0.2,1.4-0.6l3.6-3.6l3.6,3.6   C16,18.8,16.5,19,17,19s1-0.2,1.4-0.6c0.8-0.8,0.8-2,0-2.8L14.8,12z"
+          id="exit"
+        />
+      </svg>
     </div>
   );
 };
