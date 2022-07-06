383b74ca1cdccf0d01adfbd11bbbc834c7501b37
laytep <nazar.mishchenia@gmail.com>
2022-07-06 19:06:45 +0200

    add finding algoritms

--- src/components/SearchBlock/index.jsx
+++ src/components/SearchBlock/index.jsx
@@ -1,4 +1,3 @@
-import { clear } from '@testing-library/user-event/dist/clear';
 import React from 'react';
 import styles from './Search.module.scss';
 
@@ -14,12 +13,14 @@ const Search = ({ searchValue, setSearchValue }) => {
         className={styles.input}
         placeholder="find pizza..."
       />
-      <svg className={styles.clearIcon}>
-        <path
-          d="M14.8,12l3.6-3.6c0.8-0.8,0.8-2,0-2.8c-0.8-0.8-2-0.8-2.8,0L12,9.2L8.4,5.6c-0.8-0.8-2-0.8-2.8,0   c-0.8,0.8-0.8,2,0,2.8L9.2,12l-3.6,3.6c-0.8,0.8-0.8,2,0,2.8C6,18.8,6.5,19,7,19s1-0.2,1.4-0.6l3.6-3.6l3.6,3.6   C16,18.8,16.5,19,17,19s1-0.2,1.4-0.6c0.8-0.8,0.8-2,0-2.8L14.8,12z"
-          id="exit"
-        />
-      </svg>
+      {searchValue && (
+        <svg className={styles.clearIcon} onClick={() => setSearchValue('')}>
+          <path
+            d="M14.8,12l3.6-3.6c0.8-0.8,0.8-2,0-2.8c-0.8-0.8-2-0.8-2.8,0L12,9.2L8.4,5.6c-0.8-0.8-2-0.8-2.8,0   c-0.8,0.8-0.8,2,0,2.8L9.2,12l-3.6,3.6c-0.8,0.8-0.8,2,0,2.8C6,18.8,6.5,19,7,19s1-0.2,1.4-0.6l3.6-3.6l3.6,3.6   C16,18.8,16.5,19,17,19s1-0.2,1.4-0.6c0.8-0.8,0.8-2,0-2.8L14.8,12z"
+            id="exit"
+          />
+        </svg>
+      )}
     </div>
   );
 };
