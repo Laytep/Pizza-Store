fd093fc953dcd92b4cca7bd9971e310f75301e35
laytep <nazar.mishchenia@gmail.com>
2022-07-06 17:27:26 +0200

    add search styles

--- src/components/SearchBlock/index.jsx
+++ src/components/SearchBlock/index.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 import styles from './Search.module.scss';
 
 const Search = () => {
-  return <input className={styles.root} placeholder="find pizza..." />;
+  return (
+    <div className={styles.root}>
+      <svg className={styles.icon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path d="M18,10a8,8,0,1,0-3.1,6.31l6.4,6.4,1.41-1.41-6.4-6.4A8,8,0,0,0,18,10Zm-8,6a6,6,0,1,1,6-6A6,6,0,0,1,10,16Z" />
+      </svg>
+      <input className={styles.input} placeholder="find pizza..." />
+    </div>
+  );
 };
 
 export default Search;
